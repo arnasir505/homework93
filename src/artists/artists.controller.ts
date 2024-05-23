cf103effbca4ee8121764dce5e9bb4cb2404bba1
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-05-23 16:23:58 +0600

    Install and config multer to create artist

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Artist, ArtistDocument } from 'src/schemas/artist.schema';
+import { CreateArtistDto } from './create-artist.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('artists')
 export class ArtistsController {
@@ -9,6 +18,19 @@ export class ArtistsController {
     @InjectModel(Artist.name) private artistModel: Model<ArtistDocument>,
   ) {}
 
+  @Post()
+  @UseInterceptors(FileInterceptor('image', { dest: './public/images' }))
+  async create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() artistDto: CreateArtistDto,
+  ) {
+    const artist = new this.artistModel({
+      name: artistDto.name,
+      information: artistDto.information,
+      image: file ? '/images/' + file.filename : null,
+    });
+    return await artist.save();
+  }
   @Get()
   async getAll() {
     return await this.artistModel.find();
